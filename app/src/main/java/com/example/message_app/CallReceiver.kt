c79a9c89b16bb0adac68e836b055a9b96b5e317c
Agustín Balcázar <agus.b.reyes@gmail.com>
2025-03-04 22:47:34 -0600

    Se agregaron los permisos al manifiesto, se agregó una característica de uso obligatoria para usar la aplicación debido al uso de la telefonía, se registró el brodcast receiver en el manifiesto

---
+++ app/src/main/java/com/example/message_app/CallReceiver.kt
@@ -0,0 +1,12 @@
+package com.example.message_app
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class CallReceiver: BroadcastReceiver()
+{
+    override fun onReceive(p0: Context?, intent: Intent?) {
+
+    }
+}
